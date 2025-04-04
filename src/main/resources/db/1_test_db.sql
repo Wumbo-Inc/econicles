0d57914c65359c586ddba4ea4f142e5c2ba0d3e5
Barnado <barnado@wumboinc.com>
2025-04-04 14:51:57 +0000

    Fixing duplicate table names in .sql file

--- src/main/resources/db/1_test_db.sql
+++ src/main/resources/db/1_test_db.sql
@@ -3,24 +3,17 @@ pragma synchronous = normal;
 pragma temp_store = memory;
 PRAGMA busy_timeout = 5000;
 
-create table tickers
-(
-    symbol text primary key,
-    name   text NOT NULL
-);
-
-CREATE VIRTUAL TABLE tickers_search USING fts5
-(
+CREATE VIRTUAL TABLE tickers_search USING fts5(
     symbol,
-    name,
+    name
 );
 
 create table tickers
 (
-    symbol primary key not null,
-    name               not null,
-    exchange           not null,
-    exchangeName       not null,
-    currency           not null,
-    url
+    symbol text primary key not null,
+    name text not null,
+    exchange text not null,
+    exchangeName text not null,
+    currency text not null,
+    url text
 );
